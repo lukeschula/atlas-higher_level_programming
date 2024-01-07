87a9542c3d2d998c86821d0203fe2809bb453ccc
root <schulaluke@gmail.com>
2024-01-07 12:22:00 -0800

    passed but idk if the checker will like this

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+def uppercase(str):
+    for x in range(len(str)):
+        char = ord(str[x])
+        if char >= 97 and char <= 122:
+            char = char - 32
+        print("{}".format(chr(char)), end='')
+        print()
+
