7b7335196297a8f7f93a5dc63fb6c00f5410f1e9
root <schulaluke@gmail.com>
2024-01-07 17:55:34 -0800

    edit

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,11 +2,7 @@
 
 import hidden_4
 
-def main():
-    x = dir(hidden_4)
-    for i in range(len(x)):
-        if(x[i][0] != '_'):
-            print("{}".format(x[i]))
-
-    if  __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    for i in dir(hidden_4):
+        if not i.startswith('__'):
+            print(i)
