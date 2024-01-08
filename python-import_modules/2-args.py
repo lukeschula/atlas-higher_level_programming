ae33d843dd49c4cc1ae69b4c4ee3071764de09d4
root <schulaluke@gmail.com>
2024-01-07 17:25:12 -0800

    edit

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 import sys
 
-if __name__ != "__main__":
-    exit()
-
-    arg_tmp = "{:d} argument"
-    argc = len(sys.argv) - 1
-    if argc == 0:
-        arg_tmp += 's.'
-    elif argc == 1:
-        arg_tmp += ':'
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
     else:
-        arg_tmp += 's:'
-    print(arg_tmp.forma
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for x in range(1, len(sys.argv)):
+            print("{}: {}".format(x, sys.argv[x]))
