4f5fe8754a9c75645208e201b90f40dcf601f39b
root <schulaluke@gmail.com>
2024-01-07 16:30:57 -0800

    edit

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+import hidden_4
+
+def main():
+    x = dir(hidden_4)
+    for i in range(len(x)):
+        if(x[i][0] != '_'):
+            print("{}".format(x[i]))
+
+    if  __name__ == "__main__":
+        main()
