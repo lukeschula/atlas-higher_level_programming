aea268998599f416af99daab0c1de2c1a759850d
root <schulaluke@gmail.com>
2024-01-08 12:53:47 -0800

    bug fix

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if my_list:
+        new_list = my_list.copy()
+        for x in range(0, len(my_list)):
+            if my_list[x] % 2 == 0:
+                new_list[x] = True
+            else:
+                new_list[x] = False
+        return new_list
