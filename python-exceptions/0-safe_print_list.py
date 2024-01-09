bc24444610e1ed87c0b491f5246de00990204326
root <schulaluke@gmail.com>
2024-01-09 12:28:38 -0800

    test

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    print_it = 0
+    for it in range(x):
+        try:
+            print("{:d}".format(my_list[it]), end="")
+            print_it += 1
+        except IndexError:
+            break
+    print("")
+    return print_it
