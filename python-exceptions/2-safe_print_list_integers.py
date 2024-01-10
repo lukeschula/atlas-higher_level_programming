d7c2a63199b7a3f8f5aa972469e03fb3e32ab40c
root <schulaluke@gmail.com>
2024-01-09 19:05:45 -0800

    finished first part

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def safe_print_list_integers(my_list=[], a=0):
+    print_it = 0
+    for i in range(a):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            print_it += 1
+        except (TypeError, ValueError):
+            continue
+    print("")
+    return print_it
--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        x = a / b
+    except ZeroDivisionError:
+        x = None
+        return 
+    finally:
+        print("Inside result: {}".format(x))
+        return x
