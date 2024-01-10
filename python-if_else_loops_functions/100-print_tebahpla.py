e68c77e1264fc0d8935cbac2fed8d3ccc481e4e7
root <schulaluke@gmail.com>
2024-01-10 09:46:41 -0800

    yay, I solved a advanced task, group effort

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,9 +2,8 @@
 
 
 for i in reversed(range(97,123)):
-        if i % 2 == 0:
-            print(chr(i), end="")
-        elif i % 2 == 1:
-            print(chr(i).upper(), end="")
+        if i % 2 == 1:
+            i -= 32
+        print(chr(i), end="")
             
 
