76f753eb5ef0cdf225b936c6634f112da34eed1a
lukeschula <schulaluke@gmail.com>
2024-01-12 16:28:25 +0000

    first test, checking progress

---
+++ python-classes/3-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''esatblishing Square class'''
+
+
+class Square:
+    '''creating a private attribute with the name size'''
+    def __init__(self, size=0):
+        '''adding logic to ensure size must be an integer'''
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        else:
+            self.__size = size
+            '''adding logic to ensure size is less than 0'''
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    '''creating a public instance called area to return area of the square'''
+    def area(self, length, width):
+        self.length = length
+        self.width = width
+        area = length * width
+
