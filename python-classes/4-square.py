8e6e5ca4814c98ffb5808a5ea38282c688fe4b35
lukeschula <schulaluke@gmail.com>
2024-01-12 17:14:28 +0000

    moved getter and put it in correct file this time

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -5,6 +5,11 @@
 class Square:
     '''creating a private attribute with the name size'''
     def __init__(self, size=0):
+        @property
+        def size(self):
+            '''property to retrieve private instance'''
+            return self.__size
+
         '''adding logic to ensure size must be an integer'''
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
