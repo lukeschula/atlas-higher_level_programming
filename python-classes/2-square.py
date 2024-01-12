6517f6b37a9bcf8f77e1abfa3cd7cf7e5696b035
lukeschula <schulaluke@gmail.com>
2024-01-12 16:14:22 +0000

    pycode fix

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,13 +4,14 @@
 
 class Square:
     '''creating a private attribute with the name size'''
-    def __init__(self,size=0):
+    def __init__(self, size=0):
         '''adding logic to ensure size must be an integer'''
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         else:
-            self.__size = size 
+            self.__size = size
+            '''adding logic to ensure size is less than 0'''
         if size < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = size 
+            self.__size = size
