36792571f2b8fa918e901670d12d518554f9aad1
lukeschula <schulaluke@gmail.com>
2024-01-13 16:42:06 +0000

    final bug fix for file 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -5,26 +5,21 @@
 class Square:
     '''creating a private attribute with the name size'''
     def __init__(self, size=0):
-        @property
-        def size(self):
-            '''property to retrieve private instance attribute'''
-            return self.__size
+        self.size = size
+    @property
+    def size(self):
+        '''property to retrieve private instance attribute'''
+        return self.__size
 
-        @size.setter
-        def size(self, value):
-            '''property to set the attribute'''
-            return self.__size
-
+    @size.setter
+    def size(self, value):
         '''adding logic to ensure size must be an integer'''
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        else:
-            self.__size = size
         '''adding logic to ensure size is less than 0'''
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-        '''creating a public instance called area to return area of the square'''
+        self.__size = value
+    '''creating a public instance called area to return area of the square'''
     def area(self):
         return self.__size * self.__size
