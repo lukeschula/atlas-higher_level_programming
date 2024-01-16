06474663d7f6fa018bb7b6dbaaaad725d4657e3d
lukeschula <schulaluke@gmail.com>
2024-01-16 22:50:44 +0000

    added str

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -1,1 +1,69 @@
 #!/usr/bin/python3
+"""shebang"""
+
+
+class Rectangle:
+    """Rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        """Rectangle attributes"""
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__width = width
+        self.__height = height
+
+    @property
+    def height(self):
+        """Retrieve data"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set the height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        """Retrieve data"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def area(self):
+        """find area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """finds perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (self.__width * 2) + (self.__height * 2)
+
+    def __str__(self):
+        """rectangle printed w/ #"""
+        pr = ""
+        if self.__width != 0 and self.__height != 0:
+            for x in range(self.__height):
+                for y in range(self.__width):
+                    pr += "#"
+                if x < self.__height - 1:
+                    pr += "\n"
+        return pr
