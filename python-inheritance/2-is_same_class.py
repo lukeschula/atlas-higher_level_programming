b945a550fc201f30e53430ead90bfee112661d1e
lukeschula <schulaluke@gmail.com>
2024-01-22 16:53:31 +0000

    pycode fix

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -2,6 +2,7 @@
 """function that returns True if the object is exactly an
 instance of the specified class ; otherwise False"""
 
+
 def is_same_class(obj, a_class):
     """return true or false"""
 
