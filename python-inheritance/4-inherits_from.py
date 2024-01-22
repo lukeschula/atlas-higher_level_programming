ccf9ad1c5d2df5868c0b8eab8a7dbfe3f948df4b
lukeschula <schulaluke@gmail.com>
2024-01-22 17:53:28 +0000

    changed and to or

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,7 +5,7 @@
 
 def inherits_from(obj, a_class):
     """checks if object returns True or False"""
-    if type(obj) is a_class and not isinstance(obj, a_class):
+    if type(obj) is a_class or not isinstance(obj, a_class):
         return False
     else:
         return isinstance(obj, a_class)
