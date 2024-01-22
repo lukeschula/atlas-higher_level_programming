247e43afec1304a409161e7fd558893f904819c5
lukeschula <schulaluke@gmail.com>
2024-01-22 23:38:21 +0000

    added 7 basegeometry logic

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 """a class Rectangle that inherits from BaseGeometry"""
 
+class BaseGeometry:
+    """adding public instance method, that raises an Exception"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
     """Instantiation with width and height"""
