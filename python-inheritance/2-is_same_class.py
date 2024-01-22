1e911489a9875b69c39c2266beb6cbb7ca6da546
lukeschula <schulaluke@gmail.com>
2024-01-22 16:52:12 +0000

    fix syntx error, and closed parenthesis in main file

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -5,7 +5,7 @@ instance of the specified class ; otherwise False"""
 def is_same_class(obj, a_class):
     """return true or false"""
 
-    if obj is type(a_class)
+    if type(obj) is a_class:
         return True
     else:
         return False
