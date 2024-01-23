3fcb63ccc7cb9e6776848bc0a7945afc3399a785
lukeschula <schulaluke@gmail.com>
2024-01-23 17:24:35 +0000

    fixed typo, check time

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""a function that returns the JSON representation of an object"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """returns the JSON representation, (string)"""
+
+    return json.dumps(my_obj)
