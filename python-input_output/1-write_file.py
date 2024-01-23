f786721dd4e95059abcf60c392a3ac8541264819
lukeschula <schulaluke@gmail.com>
2024-01-23 16:58:39 +0000

    edit uft to utf

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,5 +6,5 @@ def write_file(filename="", text=""):
     """"writes a string to a text file,
     and returns the number of characters written"""
 
-    with open(filename, "w", encoding="uft-8") as f:
-       return f.write(text)
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
