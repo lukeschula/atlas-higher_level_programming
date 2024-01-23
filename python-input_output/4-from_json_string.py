ddbb663ad2656dfb4e68cc0f7fe4be1a2bb1c0fd
lukeschula <schulaluke@gmail.com>
2024-01-23 17:46:28 +0000

    forgot to import json

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """a function that returns an object (Python data structure)"""
 
+import json
+
 
 def from_json_string(my_str):
     """returns an object (Python data structure) r
