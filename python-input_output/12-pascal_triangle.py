08bc753781916c689678995bdb2a21a54c6f203e
lukeschula <schulaluke@gmail.com>
2024-01-23 23:57:57 +0000

    pycode fix

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -6,7 +6,7 @@ def pascal_triangle(n):
     """prototype"""
     if n <= 0:
         return []
-    pas_tri = [[1 for column in range(row)]for row in range (1, n +1)]
+    pas_tri = [[1 for column in range(row)] for row in range(1, n +1)]
 
     for x in range(2, n):
         for y in range(1, x):
