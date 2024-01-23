082c999a0fdfd3ce842e9331151128eb31795892
lukeschula <schulaluke@gmail.com>
2024-01-23 22:12:44 +0000

    check

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Write a class Student that defines a student"""
+
+
+class Student:
+    """student class"""
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def class_to_json(obj):
+        """returns the dictionary description with simple data structure"""
+        return obj.__dict__
