33138e3b339ee3f0599e6c5f9a0aeeba964360a9
lukeschula <schulaluke@gmail.com>
2024-01-23 16:55:24 +0000

    forgot to put w in open statement

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,5 +6,5 @@ def write_file(filename="", text=""):
     """"writes a string to a text file,
     and returns the number of characters written"""
 
-    with open(filename, encoding="uft-8") as f:
+    with open(filename, "w", encoding="uft-8") as f:
         f.write(text)
