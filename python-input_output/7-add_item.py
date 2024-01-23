43c59adc11757c08cce4a58ab189836102f26982
lukeschula <schulaluke@gmail.com>
2024-01-23 19:39:57 +0000

    gave permission to file 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,3 +7,7 @@ save_to_json_file =__import__('5-save_to_json_file').save_to_json_file
 load_from_json_file =__import__('6-load_from_json_file').load_from_json_file
 
 
+if isfile("add_item.json"):
+    new_list = load_from_json_file("add_item.json")
+else:
+    new_list = []
