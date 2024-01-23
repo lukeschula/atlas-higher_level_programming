2ef45ab17ac7092e50c57eb88ac566ede575c159
lukeschula <schulaluke@gmail.com>
2024-01-23 16:21:43 +0000

    added UTF8

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """reads a text file, and prints it to stdout"""
-    with open(filename, 'r') as f:
+    with open(filename, 'r', encoding='UTF8') as f:
         print(f.read())
