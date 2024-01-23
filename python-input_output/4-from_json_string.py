40ca25f030ae9c58d55986ff8c0d7cbd748109da
lukeschula <schulaluke@gmail.com>
2024-01-23 17:42:24 +0000

    check

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""a function that returns an object (Python data structure)"""
+
+
+def from_json_string(my_str):
+    """returns an object (Python data structure) r
+    epresented by a JSON string"""
+
+    return (json.dumps(my_str))
