88195d28cbb223acb9a34e08547446a0bd80a823
lukeschula <schulaluke@gmail.com>
2024-01-23 23:56:24 +0000

    check, potentially last one

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""returns a list of lists of integers representing the Pascals triangle"""
+
+
+def pascal_triangle(n):
+    """prototype"""
+    if n <= 0:
+        return []
+    pas_tri = [[1 for column in range(row)]for row in range (1, n +1)]
+
+    for x in range(2, n):
+        for y in range(1, x):
+            pas_tri[x][y] = pas_tri[x - 1][y] + pas_tri[x - 1][y - 1]
+    return pas_tri
