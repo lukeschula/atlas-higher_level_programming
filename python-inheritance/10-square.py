7cb7325804a5c6450955dd19b291ff95aa7ebea4
lukeschula <schulaluke@gmail.com>
2024-01-23 00:47:07 +0000

    used super method instead of boiler plate code

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -11,10 +11,4 @@ class Square(Rectangle):
         self.__size = size
         self.integer_validator("size", size)
 
-    def area(self):
-        """calculates area"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """prints string"""
-        return "[Rectangle] {}/{}".format(self._Square__size, self._Square__size)
+        super().__init__(size, size)
