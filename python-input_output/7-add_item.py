362ab27837cfee2dfa1fb44ad12527e5f101e918
lukeschula <schulaluke@gmail.com>
2024-01-23 20:11:57 +0000

    pycode fix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,8 +5,8 @@ from sys import argv
 import os
 import json
 
-save_to_json_file =__import__ ('5-save_to_json_file').save_to_json_file
-load_from_json_file =__import__ ('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 if os.path.isfile("add_item.json"):
