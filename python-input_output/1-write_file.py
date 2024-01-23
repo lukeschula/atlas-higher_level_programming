9801700583d28f5605cb4d9b15093ef1ade995c1
lukeschula <schulaluke@gmail.com>
2024-01-23 16:41:41 +0000

    check

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""a function that writes a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """"writes a string to a text file,
+    and returns the number of characters written"""
+
+    with open(filename, encoding='uft-8') as f:
+        file = open('my_first_file.txt', 'w')
+        file.write("This School is so cool!\n")
