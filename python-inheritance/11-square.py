c9844f521f0d9e9b86b3e4960129aa411c0545f9
lukeschula <schulaluke@gmail.com>
2024-01-23 02:14:41 +0000

    check

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""a class Square that inherits from Rectangle"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class initialized"""
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", size)
+
+        super().__init__(size, size)
+        """calculate area with super function"""
+
+    def __str__(self):
+        """prints string"""
+        return "[Square] {}/{}".format(self.__size)
