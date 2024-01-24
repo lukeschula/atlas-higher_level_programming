c75e6fac19addfc1428bb31a37275aa4e3ff1883
lukeschula <schulaluke@gmail.com>
2024-01-24 04:12:19 +0000

    import 7 file edit

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,21 +2,13 @@
 """a class Rectangle that inherits from BaseGeometry"""
 
 
-class BaseGeometry:
-    """adding public instance method, that raises an Exception"""
-    def area(self):
-        raise Exception("area() is not implemented")
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""imort file 7"""
 
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
     """Instantiation with width and height"""
     def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
+        self.__width = self.integer_validator("width", width)
+        self.__height = self.integer_validator("height", height)
+
