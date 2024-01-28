0a492218fc61bc16f98dd94dc7d7235b482af35a
lukeschula <schulaluke@gmail.com>
2024-01-28 19:25:44 +0000

    reordering files

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,13 +2,14 @@
 """"almost a circle start"""
 
 
+__nb_objects = 0
 
 class Base:
     """writing the first class, Base"""
     def __init__(self, id=None):
         """class constructor"""
-        self.id = id
-        __nb_objects = 0
-
-        if id != None:
-            id =
+        if id is not None:
+            self.id = id
+        else:
+            __nb_objects += 1
+            self.id = __nb_objects
