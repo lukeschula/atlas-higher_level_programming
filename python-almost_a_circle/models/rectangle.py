48a289012ecbcc55562533931aa38ef9666a31f7
lukeschula <schulaluke@gmail.com>
2024-01-28 20:59:17 +0000

    check to see if I'm on right track

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -23,6 +23,10 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -31,6 +35,10 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must an intege")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__height = value
 
     @property
