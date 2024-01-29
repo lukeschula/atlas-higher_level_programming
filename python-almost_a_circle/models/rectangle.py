ee0918c86e07756a0d7b3ab685c80ea07f780bc0
lukeschula <schulaluke@gmail.com>
2024-01-29 16:36:43 +0000

    one last pycode fix

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -79,4 +79,5 @@ class Rectangle(Base):
 
     def __str__(self):
         return ("[Rectangle] ({}) {}/{} - {}/{}"
-                .format(self.id, self.__x, self.__y, self.__width, self.__height))
+                .format(self.id, self.__x, self.__y,
+                        self.__width, self.__height))
