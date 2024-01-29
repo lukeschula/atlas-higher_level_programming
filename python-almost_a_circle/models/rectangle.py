23914b475d321aba2109ccc7d4b33dfd84449ca6
lukeschula <schulaluke@gmail.com>
2024-01-29 16:32:54 +0000

    instead of assigning output, I used it as a return

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -76,3 +76,6 @@ class Rectangle(Base):
             print()
         for j in range(self.__height):
             print(" " * self.__x + "#" * self.__width)
+
+    def __str__(self):
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height))
