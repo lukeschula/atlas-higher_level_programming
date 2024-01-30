c169c8ea1fa8b910a0e493000c5b9fcdadffb3e0
lukeschula <schulaluke@gmail.com>
2024-01-30 20:30:36 +0000

    switched attributes around

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -7,7 +7,7 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Square class Constructor"""
-        super().__init__(size, size, id, x, y)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """overloading __str__ method should return
