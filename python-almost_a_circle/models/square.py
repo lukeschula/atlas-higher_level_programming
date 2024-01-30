d18e0d586549c939f10337dcc60c3685a5a28234
lukeschula <schulaluke@gmail.com>
2024-01-30 22:56:29 +0000

    check

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -10,6 +10,19 @@ class Square(Rectangle):
         """Square class Constructor"""
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        return self.size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """overloading __str__ method should return
         [Square] (<id>) <x>/<y> - <size>"""
