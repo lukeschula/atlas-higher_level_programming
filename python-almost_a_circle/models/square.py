1985fe881d11306798a02af5701e4a7bcd225e3c
lukeschula <schulaluke@gmail.com>
2024-01-30 23:24:39 +0000

    check

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -38,3 +38,6 @@ class Square(Rectangle):
         elif kwargs:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
