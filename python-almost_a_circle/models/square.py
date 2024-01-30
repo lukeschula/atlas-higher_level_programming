c210b35f3ef0b7694042a63259177cb43b5ad8da
lukeschula <schulaluke@gmail.com>
2024-01-30 20:28:28 +0000

    added str magic method

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -8,3 +8,8 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Square class Constructor"""
         super().__init__(size, size, id, x, y)
+
+    def __str__(self):
+        """overloading __str__ method should return
+        [Square] (<id>) <x>/<y> - <size>"""
+        return ("[Square] ({}) {}/{} - {}").format(id, self.x, self.y, self.width, self.height)
