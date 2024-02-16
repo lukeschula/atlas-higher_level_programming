f3d4e8db379da0d6bf0d71b567f82df940cf483f
lukeschula <schulaluke@gmail.com>
2024-02-16 23:24:00 +0000

    added name

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- Creates a table called first_table
 -- first_table description: id INT name VARCHAR(256)
-CREATE TABLE IF NOT EXISTS first_table (id INT, VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
