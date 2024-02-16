37295d3938bf5211032990a3a330ab40b88056e7
lukeschula <schulaluke@gmail.com>
2024-02-16 23:14:35 +0000

    rewrote, still no it

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- Creates a table called first_table
 -- first_table description: id INT name VARCHAR(256)
-CREATE TABLE IF EXISTS first_table (INT, VARCHAR(256));
+CREATE TABLE IF EXISTS first_table (name VARCHAR(256)), id INT;
