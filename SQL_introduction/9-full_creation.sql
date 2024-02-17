4bfd70f9b55f46e90ca55a8649be8bd73b364bed
lukeschula <schulaluke@gmail.com>
2024-02-17 20:52:07 +0000

    added INSERT INTO statement, and places values of rows in it

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,9 @@
 -- Creates a table second_table in the database hbtn_0c_0
 -- Descriptionand rows given, script should fail if table already exists
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT) VALUES (
-    "1", "John", "10", "2", "Alex", "3", "3", "Bob", "14", "4", "George", "8"
-);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+
+INSERT INTO second_table (id, name, score) VALUES
+("1", "John", "10"),
+("2", "ALEX", "3"),
+("3", "Bob", "14"),
+("4", "George", "8");
