d804a2ae2a9698048c9d8fac3c67b7b0f7ef8f34
lukeschula <schulaluke@gmail.com>
2024-02-17 21:03:02 +0000

    plced score and name in *, added ORDER BY AND DESC

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,3 +1,4 @@
 -- Lists all records of the table second_table
 -- Should display both the score and the name (in this order)
-SELECT * FROM second_table VALUES (score, name);
+SELECT score, name FROM second_table
+ORDER BY score DESC;
