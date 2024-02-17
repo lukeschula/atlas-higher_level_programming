aa5854c3fdfa709b1f863cee13a7d734e5b0ab8f
lukeschula <schulaluke@gmail.com>
2024-02-17 21:10:27 +0000

    removed second semi-colon

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,4 +1,4 @@
 -- Lists all records with a score >= 10 in the table second_table
 -- Should display score and name, should be ordered by score
-SELECT score , name FROM second_table WHERE score >= 10;
+SELECT score, name FROM second_table WHERE score >= 10
 ORDER BY score DESC;
