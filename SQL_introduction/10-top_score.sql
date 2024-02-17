3a9b8c614dcda05e17391c51f916f97bb8cbe2b5
lukeschula <schulaluke@gmail.com>
2024-02-17 20:59:30 +0000

    added VALUES statement

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- Lists all records of the table second_table
 -- Should display both the score and the name (in this order)
-SELECT * FROM second_table;
+SELECT * FROM second_table VALUES (score, name);
