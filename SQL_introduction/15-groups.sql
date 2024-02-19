6cdc4882e6fbe45bcb56665e85996984204430ec
lukeschula <schulaluke@gmail.com>
2024-02-19 15:47:43 +0000

    restructure

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 --  Lists the number of records with the same score in the table
-SELECT score, COUNT(score)
-AS number
+SELECT score, COUNT(*) AS number
 FROM second_table
+GROUP BY score
 GROUP BY score DESC;
