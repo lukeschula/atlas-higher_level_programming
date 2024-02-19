30b8bbed91c9e0a1d369f82d67f78751bd2afcd4
lukeschula <schulaluke@gmail.com>
2024-02-19 16:18:48 +0000

    added quotations

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Lists all privileges of the MySQL users
-SHOW GRANTS FOR user_0d_1@localhosts;
-SHOW GRANTS FOR user_0d_2@localhosts;
+SHOW GRANTS FOR "user_0d_1"@"localhosts";
+SHOW GRANTS FOR "user_0d_2"@"localhosts";
