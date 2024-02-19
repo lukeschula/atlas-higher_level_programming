ac1e48868b4563c82768a3b996100a4c5d0a7d48
lukeschula <schulaluke@gmail.com>
2024-02-19 15:45:47 +0000

    edit

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,4 +1,5 @@
 --  Lists the number of records with the same score in the table
 SELECT score, COUNT(score)
 AS number
-FROM second_table GROUP BY score DESC;
+FROM second_table
+GROUP BY score DESC;
