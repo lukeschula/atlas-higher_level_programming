95efd3d794f7097e3fc3c4655ee1bde376d48cf7
lukeschula <schulaluke@gmail.com>
2024-02-20 15:17:00 +0000

    edit

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,5 @@
 -- Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
 SELECT sht.title, shg.genre_id FROM tv_shows AS sht
-JOIN tv_shows_genres AS shg ON sht.id = shg.show_id
+JOIN tv_shows_genres AS shg
+    ON sht.id = shg.show_id
 ORDER BY sht.title, shg.genre_id;
