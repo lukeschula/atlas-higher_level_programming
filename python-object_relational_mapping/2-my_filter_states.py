6032d7d6be8354326e76d5a6d9edaa4250ea5c5b
lukeschula <schulaluke@gmail.com>
2024-02-21 21:19:07 +0000

    pycode fix

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
         charset="utf8")
     crsr = connection.cursor()
     crsr.execute("SELECT * FROM states WHERE name LIKE binary '{}'"
-                .format(matchName))
+                 .format(matchName))
     q_rows = crsr.fetchall()
     for row in q_rows:
         print(row)
