1abc829fede0e5a2017746363e89c18625d118ea
lukeschula <schulaluke@gmail.com>
2024-02-21 17:35:40 +0000

    check

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""lists all states with a name sarting with N"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306, user=username,
+        passwd=password, db=database,
+        charset="utf8")
+    crsr = connection.cursor()
+    crsr.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'")
+    q_rows = cur.fetchall()
+    for row in q_rows:
+        print(row)
+    crsr.close()
+    connection.close()
