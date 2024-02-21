edabae59ca296b6f5995dfdb404622b2d1cd8f87
lukeschula <schulaluke@gmail.com>
2024-02-21 17:45:13 +0000

    check

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""takes in an argument and displays all values in the states table"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    matchName = sys.argv[4]
+
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306, username=username,
+        passwd=password, db=database,
+        charset="utf8")
+    crsr = connection.cursor()
+    crsr.execute("SELECT * FROM states WHERE name LIKE binary '{}'")
+
+    q_rows= cur.fetchall()
+    for row in q_rows:
+        print(row)
+    crsr.close()
+    connection.close()
