c0b7f78338250436b947c6d8f5d9bdb4f7f06e0f
lukeschula <schulaluke@gmail.com>
2024-03-26 12:33:21 -0500

    spaces instead of indent

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -3,9 +3,9 @@
 const { argv } = require('process');
 const numofArgs = argv.length;
 if (numofArgs <= 2) {
-    console.log('No argument');
+  console.log('No argument');
 } else if (numofArgs === 3) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
