163a16521c39e3fd23cac7b752ee63021f81cc5c
lukeschula <schulaluke@gmail.com>
2024-03-26 12:27:35 -0500

    push local code to github

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+const numofArgs = argv.length;
+
+if (numofArgs <= 2) {
+    console.log('No argument');
+} else if (numofArgs === 3) {
+    console.log('Argument found');
+} else {
+    console.log('Arguments found');
+}
