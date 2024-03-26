8db59cdf25a202cd6f8107291fc777112315f1f0
lukeschula <schulaluke@gmail.com>
2024-03-26 14:03:27 -0500

    forgot to sync

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+
+function factors (int) {
+    if (isNaN(int)) {
+        int = 1;
+    }
+    if (int === 1) {
+        return 1;
+    } else {
+      return int * factors(int - 1);
+    }
+}
+
+const num = parseInt(argv[2]);
+const display = factors(num);
+console.log(display);
