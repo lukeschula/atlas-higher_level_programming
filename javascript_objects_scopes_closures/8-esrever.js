4301c1fd4cce8d440efbf542328c64a0055c4b2d
lukeschula <schulaluke@gmail.com>
2024-03-26 21:32:10 -0500

    semi fix

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -2,8 +2,8 @@
 
 exports.esrever = function (list) {
   const revList = [];
-    for (let x = list.length - 1; x >= 0; x--) {
-      revList.push(list[x]);
-    }
+  for (let x = list.length - 1; x >= 0; x--) {
+    revList.push(list[x]);
+  }
     return revList;
 };
