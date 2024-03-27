7f4b9aef1a12ef1a1fb5ee3807a1fcb363ac5710
lukeschula <schulaluke@gmail.com>
2024-03-26 20:55:15 -0500

    push to git after testing locally

---
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let x = 0;
+  for (let y = 0; y < list.length; y++) {
+    if (list[y] === searchElement) {
+        y++;
+    }
+  }
+  return x;
+};
