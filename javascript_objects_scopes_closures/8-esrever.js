0795ae21c8ef4a71000b0472c463917a8d4b4c4b
lukeschula <schulaluke@gmail.com>
2024-03-26 20:58:38 -0500

    push to git after testing locally

---
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+    const revList = [];
+    for (let x = list.length - 1; x >= 0; x--) {
+        revList.push(list[x]);
+    }
+    return revList;
+};
