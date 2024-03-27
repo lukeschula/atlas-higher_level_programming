faf81895385ae0e09789f34538a37b749d03d551
lukeschula <schulaluke@gmail.com>
2024-03-26 20:36:35 -0500

    push to git after testing locally

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const recallSquare = require('./5-square');
+
+class Square extends recallSquare {
+  charPrint (c) {
+    const x = !c ? 'X' : c;
+    for (let y = 0; y < this.height; y++) {
+        console.log(x.repeat(this.width));
+    }
+  }
+}
+module.exports = Square;
