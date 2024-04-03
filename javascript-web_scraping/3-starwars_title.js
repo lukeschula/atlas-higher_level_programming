596cbc0574ff55706a64cc185830f45e9d996f5d
lukeschula <schulaluke@gmail.com>
2024-04-02 21:49:25 -0500

    variable edit

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -4,9 +4,9 @@ const episode = process.argv[2];
 const Url = 'https://swapi-api.hbtn.io/api/films/' + episode;
 const request = require('request');
 
-request(Url, function (err, display, body) {
-  if (err) {
-    console.err('error:', error);
+request(Url, function (error, display, body) {
+  if (error) {
+    console.error('error:', error);
   } else {
     console.log(JSON.parse(body).title);
   }
