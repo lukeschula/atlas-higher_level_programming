8ac1c12724796ed5e569f77bf72143531047f75b
lukeschula <schulaluke@gmail.com>
2024-04-02 21:29:19 -0500

    variable error fix

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,11 +3,11 @@
 const request = require('request');
 const Url = process.argv[2];
 
-request.get(Url , (err, display) => {
+request.get(Url , (err, response) => {
   if (err) {
     console.log(err);
   } else {
-    console.log(`code: ${dislpy.statusCode}`);
+    console.log(`code: ${response.statusCode}`);
   }
 
 });
