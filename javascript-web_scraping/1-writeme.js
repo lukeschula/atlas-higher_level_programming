d2dd36a72fb558c9d61ff66a6bddd87551723900
lukeschula <schulaluke@gmail.com>
2024-04-02 20:20:09 -0500

    found bug?

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+
+const filePath = process.argv[2];
+const str = process.argv[3];
+const fs = require('fs');
+
+fs.writeFile(filePath, str, function (err) {
+  if (err) throw err;
+});
