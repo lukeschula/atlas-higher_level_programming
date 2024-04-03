28cb4f6d2ef3ee4d34d80b0936e6ff78b610e853
lukeschula <schulaluke@gmail.com>
2024-04-02 21:58:57 -0500

    semi fix

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,12 +3,10 @@
 const request = require('request');
 const Url = process.argv[2];
 
-request.get(Url , (err, response) => {
+request.get(Url, (err, response) => {
   if (err) {
     console.log(err);
   } else {
     console.log(`code: ${response.statusCode}`);
   }
-
 });
-
