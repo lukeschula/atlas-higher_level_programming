c9eecafd94295f9b0d648234681ff14a6573377d
lukeschula <schulaluke@gmail.com>
2024-04-02 21:57:06 -0500

    semi fix

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -4,19 +4,19 @@ const request = require('request');
 const Url = process.argv[2];
 
 request(Url, (error, response) => {
-    if (error) {
-      console.error(error);
-    }
-    const data = JSON.parse(response.body);
-    const newTask = {};
-    for (const task of data) {
-      if (task.completed) {
-        if (newTask[task.userId]) {
-          newTask[task.userId]++;
-        } else {
-          newTask[task.userId] = 1;
-        }
+  if (error) {
+    console.error(error);
+  }
+  const data = JSON.parse(response.body);
+  const newTask = {};
+  for (const task of data) {
+    if (task.completed) {
+      if (newTask[task.userId]) {
+        newTask[task.userId]++;
+      } else {
+        newTask[task.userId] = 1;
       }
     }
-    console.log(newTask);
+  }
+  console.log(newTask);
 });
